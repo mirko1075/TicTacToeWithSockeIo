574e519cbb367c9b8543e2dd60e59e7a51f9050f
Mirko Siddi <mirko.siddi@gmail.com>
2021-10-02 17:42:18 +0200

    First commit, Node + Express + Socket setup server

---
+++ index.js
@@ -0,0 +1,8 @@
+let express = require("express");
+let app = express();
+app.use(express.static('public'));
+const port = 5000
+let http = require('http').createServer(app);
+let io = require('socket.io')(http)
+
+http.listen(port, () => console.log("Server is listening on port " + port))
